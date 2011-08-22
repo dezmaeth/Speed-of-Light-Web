405e038090c0f32dc80a443c85e4a9cc69e4240f
Francisco Henseleit <francisco.henseleit@mcast.cl>
2011-08-22 12:31:38 -0400

    Adding movement

--- js/script.js
+++ js/script.js
@@ -5,11 +5,12 @@
 
 
 var sMain = new Object;
+sMain.player = new Object;
 sMain.ptcls = new Array;
 sMain.container = new Object;
 sMain.numPtcl = 10;
 sMain.render = false;
-sMain.fps = 30;
+sMain.fps = 15;
 sMain.container.w = 800;
 sMain.container.h = 600;
 sMain.constructor = function(key) {
@@ -19,26 +20,31 @@ sMain.constructor = function(key) {
  		canvas.height = sMain.container.h;
  		ctx = canvas.getContext('2d');
  		document.getElementById('main').appendChild(canvas);		
-		/* create ptcls */
-		for (i=0;i<sMain.numPtcl;i++) {
-		sMain.createPtcl(i);	
-		} 		
+		/* init */	
+		sMain.spawnPlayer();
+		sMain.spawnPtcls();
+		sMain.startRendering();		
  	}
 }
 
 sMain.startRendering = function() {
+	sMain.render = true;
 	sMain.t=setInterval("sMain.drawFrame()",(1000/sMain.fps));	
 }
 
 sMain.drawFrame = function() {
+	if (sMain.render) {
 	    console.log("draw");
 		ctx.fillRect(0,0,sMain.container.w,sMain.container.h);
 		sMain.updatePtcl();
-		sMain.renderParticles();	
+		sMain.renderParticles();
+		
+	}	
 }
 
 sMain.stopRendering = function() {
 	clearInterval(sMain.t);
+	sMain.render = false;
 }
 
 sMain.renderParticles = function () {
@@ -48,25 +54,42 @@ sMain.renderParticles = function () {
 }
 sMain.updatePtcl = function() {
 	for (var i in sMain.ptcls) {
-		sMain.ptcls[i].x = Math.random()*sMain.container.w; 
-		sMain.ptcls[i].y = Math.random()*sMain.container.h; 
+		sMain.ptcls[i].x += 2;
+		if (sMain.ptcls[i].x>(sMain.player.x+50))
+		sMain.ptcls[i].y += (Math.floor(Math.random()*5)*sMain.ptcls[i].d);
+		else 
+		sMain.ptcls[i].y += (Math.floor(Math.random()*5)*(sMain.ptcls[i].d));
 	}
 }
 
 sMain.createPtcl = function(i) {
 		/* ptcl init */
 		sMain.ptcls[i] = new Object;
- 		sMain.ptcls[i].x = Math.random()*sMain.container.w;
-		sMain.ptcls[i].y = Math.random()*sMain.container.h;
+		if((i % 2) == 0) 
+		sMain.ptcls[i].d = -1;
+		else 
+		sMain.ptcls[i].d = 1;
+ 		sMain.ptcls[i].x = sMain.player.x+(i*10);
+		sMain.ptcls[i].y = sMain.player.y;
 		sMain.ptcls[i].v = 1;
 		sMain.ptcls[i].w = 10;
 		sMain.ptcls[i].h = 10;
 		/* resource */
 		img = new Image;
-		img.src= "img/star_mini.png";
+		img.src= "img/star_mini_ani.gif";
 		sMain.ptcls[i].img = img;
 }
 
+sMain.spawnPlayer = function() {
+	sMain.player.x = 100;
+	sMain.player.y = 300;
+}
+
+sMain.spawnPtcls = function() {
+	for (i=0;i<sMain.numPtcl;i++) {
+		sMain.createPtcl(i);	
+	} 	
+}
 
 $(document).ready(function() { 
 	sMain.constructor("thereuare");
