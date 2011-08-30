c6006d99eb35163c945bdfd1e8c35ec9ccfdd1c6
Francisco Henseleit <francisco.henseleit@mcast.cl>
2011-08-30 15:03:29 -0300

    Added controlled light and position changes, and a FPS counter

--- js/controls.js
+++ js/controls.js
@@ -1,23 +1,42 @@
 function Controls(player) {
 	this.mouseX = 0; 
 	this.mouseY = 0;
+	this.canvasMinX = 0;
+	this.canvasMinY = 0;
+	this.canvasMaxX = 0;
+	this.canvasMaxY = 0;
 	this.init =  function() {
 	 document.onkeydown = this.key_event;
      document.onkeypress = this.key_event;
-     document.addEventListener( 'mousemove', this.mousemove, false );
+     document.addEventListener( 'mousemove', this.onMouseMove, false );
 	}
 	
-	this.mousemove = function ( event ) { 
-				this.mouseX = event.clientX;
-				this.mouseY = event.clientY;
-				pointLight.position.x = this.mouseX;
-				pointLight.position.y = this.mouseY;
+
+	this.onMouseMove = function(evt) {
+		evt.preventDefault();
+		this.canvasMinX = $("canvas").offset().left;
+  		this.canvasMinY = $("canvas").offset().top;
+  		this.canvasMaxX = this.canvasMinX + sMain.canvas.WIDTH;
+  		this.canvasMaxY = this.canvasMinY + sMain.canvas.HEIGHT;
+  		// checkeo que este dentro del canvas
+  		if (evt.pageX > this.canvasMinX && evt.pageX < this.canvasMaxX && evt.pageY > this.canvasMinY && evt.pageY < this.canvasMaxY) {
+	    
+	    light.position.x = evt.pageX - (this.canvasMinX + (sMain.canvas.WIDTH/2));
+		light.position.y = (evt.pageY - (this.canvasMinY + (sMain.canvas.HEIGHT/2)))*-1;
+	  	}
+	  		
 	}
+
 	this.key_event = function(evt)
 	{
+		console.log('key:'+evt.keyCode);
 		if (evt.keyCode==38)
 			sMain.player.position.y -= 10;
 		if (evt.keyCode==40)
 			sMain.player.position.y += 10; 
+		if (evt.keyCode==34)
+			sMain.camera.position.z -= 5;
+			sMain.camera.position.y += 5;
+			
 	}
 }
--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@ var sMain = new Object;
 	sMain.interval;
 	sMain.canvas.WIDTH = 800;
 	sMain.canvas.HEIGHT = 600;
+	sMain.walls = new Array;
 	sMain.fps = 60;
+	sMain.currentFPS = 0;
 	sMain.render = false;
 	//creamos una camara webgl, un gestor de rendering y la escena
 	sMain.renderer = new THREE.WebGLRenderer();
-	sMain.camera = new THREE.Camera(45,(sMain.canvas.WIDTH / sMain.canvas.HEIGHT),0.1,10000); 
+	sMain.camera = new THREE.Camera(50,(sMain.canvas.WIDTH / sMain.canvas.HEIGHT),0.5,1000); 
 	sMain.scene = new THREE.Scene();
 	sMain.container = $('#container');
 	// iniciamos la camara a cierta distancia
 	sMain.camera.position.z = 600;
+//	sMain.camera.position.y = -845; //vista de lado
 	
 	sMain.renderer.setSize(sMain.canvas.WIDTH, sMain.canvas.HEIGHT);
 	$('#container').append(sMain.renderer.domElement);
@@ -19,50 +22,71 @@ var sMain = new Object;
 	sMain.renderCanvas = function () {
 		if (sMain.render) {
 		sMain.renderer.render(sMain.scene, sMain.camera);
+		sMain.currentFPS ++;
 		}
 	}
 	
 	sMain.startRendering = function () {	
 		sMain.render = true;
 		sMain.interval = setInterval('sMain.renderCanvas()',1000/sMain.fps);
+		sMain.showFPS = setInterval('sMain.drawFPS()',1000);
+	}
+	sMain.drawFPS = function () {
+		$("#fps").html(sMain.currentFPS);
+		sMain.currentFPS = 0;
 	}
 	
 	sMain.stopRendering = function () {
 		sMain.render = false;
+		clearInterval(sMain.interval);
+		clearInterval(sMain.showFPS);
 	}
 	
 	// Creamos objetos
-	var sphereMaterial = new THREE.MeshLambertMaterial(	{ color: 0xFFD700 });
+	var sphereMaterial = new THREE.MeshLambertMaterial(	{ color: 0xFFFFFF });
 	
 	//variables de la esfera
-	var radio = 20, segmentos = 16, anillos= 16;
+	var radio = 40, segmentos = 32, anillos= 32;
 	
 	//creamos el mesh a partir de la geometria que vimos antes
 	sMain.player = new THREE.Mesh( new THREE.SphereGeometry(radio, segmentos, anillos),sphereMaterial);
 	
-	sMain.player.dynamic = true;
+	//sMain.player.dynamic = true;
 	// añadimos espefera a la escena
+	sMain.player.position.z = 10;
 	sMain.scene.addChild(sMain.player);
 	
 	// creamos una luz color blanca
-	var pointLight = new THREE.PointLight( 0xFFFFFF );
-	
-	
-	
+	var light = new THREE.PointLight( 0xFFFFFF );
 	// la posicionamos
-	pointLight.position.x = 10;
-	pointLight.position.y = 50;
-	pointLight.position.z = 90;
+	light.position.x = 0;
+	light.position.y = 0;
+	light.position.z = 100;
+	light.intensity = 2;	
 	
-	// la añadimos a la escena
-	sMain.scene.addChild(pointLight);
 	
 	
 	//añadir suelo
-	var floor = new THREE.Mesh( new THREE.CubeGeometry( 10000, 600, 0 ), 
-								new THREE.MeshLambertMaterial( { color: 0x000000 } )
+	var floor = new THREE.Mesh( new THREE.CubeGeometry( 10000, 300, 10 ), 
+								new THREE.MeshLambertMaterial( { color: 0xFFD700  } )
 							   );
+							 
+	sMain.addWall = function (){ 
+	var wall = new THREE.Mesh( new THREE.CubeGeometry( 20 ,100, 500 ), 
+								new THREE.MeshLambertMaterial( { color: 0xFFFFFF  } )
+							  );
+	sMain.walls.push(wall);
+	}
+	
+	sMain.drawWalls = function () {
+		for (var i in sMain.walls) 
+			sMain.scene.addChild(sMain.walls[i]);
+	}
+					
+	// añadiendo suelo
 	sMain.scene.addChild(floor);
+	// añadiendo luz
+	sMain.scene.addChild(light);
 	// comenzamos a renderizar
 	sMain.startRendering();
 	// iniciamos controles
